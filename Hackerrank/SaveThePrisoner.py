9507431b012d302e01651ec22057ec6ef4338d41
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2020-01-14 11:09:37 +0530

    Create SaveThePrisoner.py

---
+++ Hackerrank/SaveThePrisoner.py
@@ -0,0 +1,74 @@
+"""
+A jail has a number of prisoners and a number of treats to pass out to them. Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs. A chair number will be drawn from a hat. Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+
+The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful. Determine the chair number occupied by the prisoner who will receive that candy.
+
+For example, there are  prisoners and  pieces of candy. The prisoners arrange themselves in seats numbered  to . Let's suppose two is drawn from the hat. Prisoners receive candy at positions . The prisoner to be warned sits in chair number .
+
+Function Description
+
+Complete the saveThePrisoner function in the editor below. It should return an integer representing the chair number of the prisoner to warn.
+
+saveThePrisoner has the following parameter(s):
+
+n: an integer, the number of prisoners
+m: an integer, the number of sweets
+s: an integer, the chair number to begin passing out sweets from
+Input Format
+
+The first line contains an integer, , denoting the number of test cases.
+The next  lines each contain  space-separated integers:
+- : the number of prisoners
+- : the number of sweets
+- : the chair number to start passing out treats at
+
+Constraints
+
+Output Format
+
+For each test case, print the chair number of the prisoner who receives the awful treat on a new line.
+
+Sample Input 0
+
+2
+5 2 1
+5 2 2
+Sample Output 0
+
+2
+3
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the saveThePrisoner function below.
+def saveThePrisoner(n, m, s):
+    if (m+s-1)%n==0:
+        return n
+    else:
+        return (m+s-1)%n
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        nms = input().split()
+
+        n = int(nms[0])
+
+        m = int(nms[1])
+
+        s = int(nms[2])
+
+        result = saveThePrisoner(n, m, s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()

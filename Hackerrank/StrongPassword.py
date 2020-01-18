3c43309bae91306657d933e2be70979beff4572e
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2020-01-18 12:44:57 +0530

    Create StrongPassword.py

---
+++ Hackerrank/StrongPassword.py
@@ -0,0 +1,81 @@
+"""
+Louise joined a social networking site to stay in touch with her friends. The signup page required her to input a name and a password. However, the password must be strong. The website considers a password to be strong if it satisfies the following criteria:
+
+Its length is at least .
+It contains at least one digit.
+It contains at least one lowercase English character.
+It contains at least one uppercase English character.
+It contains at least one special character. The special characters are: !@#$%^&*()-+
+She typed a random string of length  in the password field but wasn't sure if it was strong. Given the string she typed, can you find the minimum number of characters she must add to make her password strong?
+
+Note: Here's the set of types of characters in a form you can paste in your solution:
+
+numbers = "0123456789"
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+special_characters = "!@#$%^&*()-+"
+Input Format
+
+The first line contains an integer  denoting the length of the string.
+
+The second line contains a string consisting of  characters, the password typed by Louise. Each character is either a lowercase/uppercase English alphabet, a digit, or a special character.
+
+Constraints
+
+Output Format
+
+Print a single line containing a single integer denoting the answer to the problem.
+
+Sample Input 0
+
+3
+Ab1
+Sample Output 0
+
+3
+Explanation 0
+
+She can make the password strong by adding  characters, for example, $hk, turning the password into Ab1$hk which is strong.
+
+ characters aren't enough since the length must be at least .
+
+Sample Input 1
+
+11
+#HackerRank
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumNumber function below.
+def minimumNumber(n, password):
+    # Return the minimum number of characters to make the password strong
+    count = 0    
+    if any(i.isdigit() for i in password)==False:
+        count+=1
+    if any(i.islower() for i in password)==False:
+        count+=1
+    if any(i.isupper() for i in password)==False:
+        count+=1
+    if any(i in '!@#$%^&*()-+' for i in password)==False:
+        count+=1
+    return max(count,6-n)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    password = input()
+
+    answer = minimumNumber(n, password)
+
+    fptr.write(str(answer) + '\n')
+
+    fptr.close()

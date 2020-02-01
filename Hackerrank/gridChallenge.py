6d4c0fe9be5bdc582f624dcfc39c7304f1120404
Aksha Ali <41459801+akshaali@users.noreply.github.com>
2020-02-01 11:12:28 +0530

    Create gridChallenge.py

---
+++ Hackerrank/gridChallenge.py
@@ -0,0 +1,99 @@
+"""
+Given a square grid of characters in the range ascii[a-z], rearrange elements of each row alphabetically, ascending. Determine if the columns are also in ascending alphabetical order, top to bottom. Return YES if they are or NO if they are not.
+
+For example, given:
+
+a b c
+a d e
+e f g
+The rows are already in alphabetical order. The columns a a e, b d f and c e g are also in alphabetical order, so the answer would be YES. Only elements within the same row can be rearranged. They cannot be moved to a different row.
+
+Function Description
+
+Complete the gridChallenge function in the editor below. It should return a string, either YES or NO.
+
+gridChallenge has the following parameter(s):
+
+grid: an array of strings
+Input Format
+
+The first line contains , the number of testcases.
+
+Each of the next  sets of lines are described as follows:
+- The first line contains , the number of rows and columns in the grid.
+- The next  lines contains a string of length 
+
+Constraints
+
+
+
+Each string consists of lowercase letters in the range ascii[a-z]
+
+Output Format
+
+For each test case, on a separate line print YES if it is possible to rearrange the grid alphabetically ascending in both its rows and columns, or NO otherwise.
+
+Sample Input
+
+1
+5
+ebacd
+fghij
+olmkn
+trpqs
+xywuv
+Sample Output
+
+YES
+Explanation
+
+The x grid in the  test case can be reordered to
+
+abcde
+fghij
+klmno
+pqrst
+uvwxy
+This fulfills the condition since the rows 1, 2, ..., 5 and the columns 1, 2, ..., 5 are all lexicographically sorted.
+"""
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the gridChallenge function below.
+def gridChallenge(grid):
+    row = len(grid)
+    column = len(grid[0])
+    for i in range(row):
+        grid[i] = sorted(grid[i], key=ord)
+    for i in range(column):
+        for j in range(row-1):
+            if ord(grid[j][i]) > ord(grid[j+1][i]):
+                return("NO")
+    return("YES")
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        grid = []
+
+        for _ in range(n):
+            grid_item = input()
+            grid.append(grid_item)
+
+        result = gridChallenge(grid)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
